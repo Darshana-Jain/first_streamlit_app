8969e82c1f945980cfff340c160a4dd4a9c9df33
Darshana-Jain <122813002+Darshana-Jain@users.noreply.github.com>
2023-01-16 22:41:12 +0530

    Update streamlit_app.py
    Add one  more heading and edit some menu item by adding emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled free range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega3 & Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔  🥑🍞Hard-boiled free range Egg')
+streamlit.text('🥑🍞 Avocoda Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
