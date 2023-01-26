1d7e442f77b1c221d1572c06f438bb32dbc33ed0
Darshana-Jain <122813002+Darshana-Jain@users.noreply.github.com>
2023-01-26 20:25:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,20 +74,35 @@ try:
                                       
 
 # import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-my_data_row = my_cur.fetchone()
-streamlit.header( "The Fruit list Load Contains:")
-streamlit.dataframe(my_data_row)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_data_row = my_cur.fetchone()
+#streamlit.text("Hello from Snowflake:")
+#streamlit.text(my_data_row)
+#my_data_row = my_cur.fetchone()
+#streamlit.header( "The Fruit list Load Contains:")
+#streamlit.dataframe(my_data_row)
 
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+#fruit load list for all
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header( "The Fruit list Load Contains:")
+#streamlit.dataframe(my_data_rows)
+                                   
+# Revised-- fruit load list for all and load into a button
+                                 
 streamlit.header( "The Fruit list Load Contains:")
-streamlit.dataframe(my_data_rows)
+#snowflake related functions:
+def get_fruit_load_list():
+with my_cnx.cursor() as my_cur: 
+my_cur.execute("select * from fruit_load_list")
+return my_cur.fetchall()
+ # add a button to load the list
+if streamlit.button('Get fruit load list') :
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])                                     
+my_data_rows = get_fruit_load_list()
+streamlit.dataframe(my_data_rows)                                   
                                    
  #allow end user to add fruit to the list
 #add_my_fruit = dtreamlit.text_input('what fruit would you like to add')  
