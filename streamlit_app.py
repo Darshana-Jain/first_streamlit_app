c91dee0047e128c6037d91a30232d88d88abe836
Darshana-Jain <122813002+Darshana-Jain@users.noreply.github.com>
2023-01-26 20:35:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,21 +56,20 @@ streamlit.dataframe(fruits_to_show)
  #Revised for Function Call / Repetable Code Block
 def get_fruitvice_data(this_fruit_choice):
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice")
-    fruitvice_normalized = pandas.json_normalize(fruitvice_response.json())
-   return fruitvice_normalized
+fruitvice_normalized = pandas.json_normalize(fruitvice_response.json())
+return fruitvice_normalized
 
    #new section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input ('what fruit would you like to information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-   else:
-      back_from_function = get_fruitvice_data(fruit_choice)  
-      streamlit.dataframe(back_from_function)                             
-    
- except urlerror as e:
-    streamlit.error()                                      
+fruit_choice = streamlit.text_input ('what fruit would you like to information about?')
+if not fruit_choice:
+streamlit.error("Please select a fruit to get information.")
+else:
+back_from_function = get_fruitvice_data(fruit_choice)  
+streamlit.dataframe(back_from_function)                             
+except urlerror as e:
+streamlit.error()                                      
                                       
 
 # import snowflake.connector
@@ -113,8 +112,8 @@ streamlit.dataframe(my_data_rows)
                                    
 # Revised - allow end user to add fruit to the list
 def insert_row_snowflake (new_fruit):
- with my_cnx.cursor() as my_cur():
-  my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+with my_cnx.cursor() as my_cur():
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 return  'Thanks for adding' + new_fruit                               
 add_my_fruit = dtreamlit.text_input('what fruit would you like to add')  
 if streamlit.button ('Add a fruit to the list'):
